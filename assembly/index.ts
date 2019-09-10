44b13621bbda1b00e8908f5f1c43eab0cfae99ea
Stefanie Doll <stefie.doll@googlemail.com>
2019-09-10 15:59:52 +0200

    simpler example

--- assembly/index.ts
+++ assembly/index.ts
@@ -1,9 +1,9 @@
-// The entry file of your WebAssembly module.
+import { u128 } from "bignum";
+// compiles to wrong sourceMap path: '~lib/bignum/integer/u128.ts'
 
-import { u128 } from "bignum/integer/u128";
+// import { u128 } from "../node_modules/bignum/assembly/integer/u128";
+// compiles to correct sourceMap path: 'node_modules/bignum/assembly/integer/u128.ts'
 
-export function add(a: i32, b: i32): Uint8Array {
+export function add(): void {
   const test = u128.from<i32>(0);
-  const u8a = test.toUint8Array();
-  return u8a;
 }
